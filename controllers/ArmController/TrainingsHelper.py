e1a2791e7b96d0ccbab8a33d9d3d075661695554
ChristianGit01 <christian@actionfactory.de>
2023-01-29 23:15:18 +0100

    testModel2

--- controllers/ArmController/TrainingsHelper.py
+++ controllers/ArmController/TrainingsHelper.py
@@ -24,22 +24,25 @@ categories = ['apple', 'orange','can','computer_mouse','hammer','beer_bottle','C
 def startTraining():
     execution_path = os.path.dirname(__file__)
     data_dir_path = os.path.join(execution_path , "DataSet")
-    model_path = os.path.join(execution_path , "Modelle/yolov3.pt")
-    createClassFiles(categories) #Erzeuge "classes.txt" anhand von categorien Liste. Erstes element "dummy" wird ausgelassen
+    model_path = os.path.join(execution_path , "Modelle/yolov3_DataSet_mixedData_epoch-50.pt")
+    createClassFiles(categories) 
     trainer = DetectionModelTrainer()
     trainer.setModelTypeAsYOLOv3()
     trainer.setDataDirectory(data_directory=data_dir_path)
-
-    trainer.setTrainConfig(object_names_array=categories, batch_size=32, num_experiments=200, train_from_pretrained_model=model_path)
+    trainer.setTrainConfig(object_names_array=categories, batch_size=32, num_experiments=30, train_from_pretrained_model=model_path)
     trainer.trainModel()
 
 def testModel():
+    execution_path = os.path.dirname(__file__)
     detector = CustomObjectDetection()
     detector.setModelTypeAsYOLOv3()
-    detector.setModelPath("yolov3_hololens-yolo_mAP-0.82726_epoch-73.pt") # path to custom trained model
-    detector.setJsonPath("hololens-yolo_yolov3_detection_config.json") # path to corresponding json
+    detector.setModelPath(os.path.join(execution_path , "Modelle/yolov3_DataSet_last.pt")) # path to custom trained model
+    detector.setJsonPath(os.path.join(execution_path , "Modelle/DataSet_yolov3_detection_config.json")) # path to corresponding json
     detector.loadModel()
-    detections = detector.detectObjectsFromImage(input_image="holo1.jpg", output_image_path="holo1-detected.jpg")
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path ,
+    'snapshot.jpg'), output_image_path=os.path.join(execution_path ,'snapshot-detected.jpg'),
+    nms_treshold = 0.1,
+    objectness_treshold = 0.6)
     for detection in detections:
         print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
@@ -122,7 +125,7 @@ def createTrainingFiles(recognizedObjectes,camera,type):
     print('File: '+fileName+' created')
 
 def moveTableNodes(supervisor,table):
-    print('Randomize objects position on table')
+    print('Randomize objects position and rotation on table')
     margin = 0.1
     bottomLeft = table.local2world([0,1,0])
     topLeft = table.local2world([0,0,0])
@@ -144,12 +147,58 @@ def moveTableNodes(supervisor,table):
         angle = random.uniform(1, 360)
         obj.getField('rotation').setSFRotation([xRotation,yRotation,zRotation,angle])
 
+def spinTableNodes(supervisor,table):
+    print('Randomize objects rotation on table')
+    for cat in categories:
+        obj = supervisor.getFromDef(cat)
+        xRotation = random.uniform(1, 360)
+        yRotation = random.uniform(1, 360)
+        zRotation = random.uniform(1, 360)
+        angle = random.uniform(1, 360)
+        obj.getField('rotation').setSFRotation([xRotation,yRotation,zRotation,angle])
+
 def generateTrainingsData(amount, supervisor, camera, table):
     for i in range(amount):
         moveTableNodes(supervisor,table) #no physics after call?
         makeSnapshot(camera,'train')
             
+def compare_directories(path1, path2):
+    files1 = []
+    for file in os.listdir(path1):
+        if file != "classes":
+            files1.append(os.path.splitext(file)[0])
+    files1 = set(files1)
+    files2 = []
+    for file in os.listdir(path2):
+        if file != "classes":
+            files2.append(os.path.splitext(file)[0])
+    files2 = set(files2)
+    only_in_path1 = files1.difference(files2)
+    only_in_path2 = files2.difference(files1)
+    if 'classes' in only_in_path1:
+        only_in_path1.remove('classes')
+    if 'classes' in only_in_path2:
+        only_in_path2.remove('classes')
+    if only_in_path1:
+        print("These files are only in", path1, ":", only_in_path1)
+    if only_in_path2:
+        print("These files are only in", path2, ":", only_in_path2)
+    if not only_in_path1 and not only_in_path2:
+        return True
+    else:
+        return False
+
+def dataSetIntegrityTest():
+    execution_path = os.path.dirname(__file__)
+    annotationPathTrain = os.path.join(execution_path , "DataSet/train/annotations/")
+    imagePathTrain = os.path.join(execution_path , "DataSet/train/images/")
+    annotationPathValidation = os.path.join(execution_path , "DataSet/validation/annotations/")
+    imagePathValidation = os.path.join(execution_path , "DataSet/validation/images/")
+    print(f"Trainings folder integrity: {compare_directories(annotationPathTrain,imagePathTrain)}")
+    print(f"Validation folder integrity: {compare_directories(annotationPathValidation,imagePathValidation)}")
 
 if __name__=="__main__":
+    #testModel()
     startTraining()
+    #dataSetIntegrityTest()
     print('DONE')
